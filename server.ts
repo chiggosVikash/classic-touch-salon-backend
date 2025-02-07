109b3f94c1b571dd1a156693eb188af2a7e2d4f7
chiggosVikash <chiggos.vikash@gmail.com>
2025-02-07 13:43:53 +0530

    update in server

--- server.ts
+++ server.ts
@@ -37,10 +37,11 @@ app.use(routes);
 // Error handler middleware
 app.use(errorHandler);
 
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/saloon_management')
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-app.listen(3001,'0.0.0.0', () => {
+app.listen(3001, () => {
   console.log(`Server is running on port ${PORT}`);
 });
